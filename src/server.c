db63bd1cc102dcc74fe7f70ffb197cad9ac459ba
DenCoder.EXE <dencoder.exe@gmail.com>
2025-04-20 19:41:56 +0200

    add cli mode

--- src/server.c
+++ src/server.c
@@ -12,11 +12,13 @@
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <pthread.h>
 #include "utils.h"
 #include "help.h"
 #include "utils.h"
 #include "colors.h"
 #include "default.h"
+#include "help.h"
 
 typedef struct DPrompt {
     char* username;
@@ -39,26 +41,35 @@ typedef struct Pipe {
 } Pipe;
 
 typedef struct Task {
-    char* tokens;
-    Pipe* pipes;
     int pipe_count;
+    Pipe* pipes;
+    char* tokens;
 } Task;
 
 typedef struct UPrompt {
     ssize_t bytes_received;
-    char client_request[SIZE];
-    Task* tasks;
+    char request[SIZE];
+    char response[SIZE*10];
     int task_count;
+    Task* tasks;
 } UPrompt;
 
 Server s;
-UPrompt u;
+UPrompt clt;
+UPrompt cli;
 DPrompt d;
+
+pthread_mutex_t mutex;
 
 static void halt(int status) {
     close(s.server_socket);
     close(s.client_socket);
     exit(status);
+}
+
+static void help(void) {
+    printf(help_header);
+    printf(help_server);
 }
 
 static void cd(char* path) {
@@ -136,12 +147,12 @@ void accept_connection(void) {
     }
 }
 
-static int receive(void) {
-    memset(u.client_request, 0, sizeof(u.client_request));  // Clear buffer
-    u.bytes_received = recv(s.client_socket, u.client_request, sizeof(u.client_request) - 1, 0);
-    if (u.bytes_received <= 0) {
+static int receive(UPrompt* u) {
+    memset(u->request, 0, sizeof(u->request));  // Clear buffer
+    u->bytes_received = recv(s.client_socket, u->request, sizeof(u->request) - 1, 0);
+    if (u->bytes_received <= 0) {
         // If recv() returns 0, the server disconnected
-        if (u.bytes_received == 0) {
+        if (u->bytes_received == 0) {
             printf(RED "Client disconnected.\n" RESET);
             close(s.client_socket);
             return -1;
@@ -155,54 +166,61 @@ static int receive(void) {
     return 0;
 }
 
-void send_dprompt(void) {
+void send_dprompt(UPrompt* u) {
     default_prompt(&d);
-    memset(s.server_response, 0, sizeof(s.server_response));  // Clear response buffer
-    sprintf(s.server_response, YELLOW "%s|%s|%s@%s|%s" RESET "\n> ", d.hostdate, d.hosttime, d.username, d.hostname, d.cwd);
-    send(s.client_socket, s.server_response, strlen(s.server_response), 0);
+    memset(u->response, 0, sizeof(u->response));  // Clear response buffer
+    sprintf(u->response, YELLOW "%s|%s|%s@%s|%s" RESET "\n> ", d.hostdate, d.hosttime, d.username, d.hostname, d.cwd);
+    send(s.client_socket, u->response, strlen(u->response), 0);
 }
 
-void clear_uprompt(void) {
-    for (int i = 0; i < u.task_count - 1; i++) {
-        if (u.tasks[i].pipes != NULL) {
-            free(u.tasks[i].pipes);
-            u.tasks[i].pipes = NULL;
+void print_dprompt(UPrompt* u) {
+    default_prompt(&d);
+    memset(u->response, 0, sizeof(u->response));  // Clear response buffer
+    printf(YELLOW "%s|%s|%s@%s|%s" RESET "\n> ", d.hostdate, d.hosttime, d.username, d.hostname, d.cwd);
+    fflush(stdout);
+}
+
+void clear_uprompt(UPrompt* u) {
+    for (int i = 0; i < u->task_count - 1; i++) {
+        if (u->tasks[i].pipes != NULL) {
+            free(u->tasks[i].pipes);
+            u->tasks[i].pipes = NULL;
         }
     }
 
-    if (u.tasks != NULL) {
-        free(u.tasks);
-        u.tasks = NULL;
+    if (u->tasks != NULL) {
+        free(u->tasks);
+        u->tasks = NULL;
     }
 }
 
-void parse_semicolon(void) {
+void parse_semicolon(UPrompt* u) {
     char* token;
     int buf_pos = 0;
     char* request_ptr;
-    u.task_count = 1;
-    u.tasks = (Task*)calloc(u.task_count, sizeof(Task));
-    if (!u.tasks) {
+    u->task_count = 1;
+    u->tasks = (Task*)calloc(u->task_count, sizeof(Task));
+    if (!u->tasks) {
         perror(RED "Error: calloc(semicolon)" RESET);
         halt(EXIT_FAILURE);
     }
 
-    token = strtok_r(u.client_request, ";", &request_ptr);
+    token = strtok_r(u->request, ";", &request_ptr);
     while (token != NULL) {
-        u.tasks[buf_pos].tokens = token;
+        u->tasks[buf_pos].tokens = token;
         buf_pos++;
 
-        if (buf_pos >= u.task_count) {
-            u.task_count++;
-            u.tasks = (Task*)realloc(u.tasks, u.task_count * sizeof(Task));
-            if (!u.tasks) {
+        if (buf_pos >= u->task_count) {
+            u->task_count++;
+            u->tasks = (Task*)realloc(u->tasks, u->task_count * sizeof(Task));
+            if (!u->tasks) {
                 perror(RED "Error: realloc(semicolon)" RESET);
                 halt(EXIT_FAILURE);
             }
         }
         token = strtok_r(NULL, ";", &request_ptr);
     }
-    u.tasks[buf_pos].tokens = NULL; 
+    u->tasks[buf_pos].tokens = NULL; 
 }
 
 void parse_pipeline(Task *task) {
@@ -234,10 +252,10 @@ void parse_pipeline(Task *task) {
     task->pipes[buf_pos].tokens = NULL; 
 }
 
-void parse_request(void) {
-    parse_semicolon();
-    for (int i = 0; i < u.task_count - 1; i++) {
-        parse_pipeline(&u.tasks[i]);
+void parse_request(UPrompt* u) {
+    parse_semicolon(u);
+    for (int i = 0; i < u->task_count - 1; i++) {
+        parse_pipeline(&u->tasks[i]);
     }
 }
 
@@ -267,7 +285,6 @@ void redirect_file(char* tokens, char* tmp_string, int* input_fd, int* output_fd
 
     for (int j = 0; redirections[j] != NULL; j++) {
         if (strcmp(redirections[j], redirection) == 0 && redirections[j+1] != NULL) {
-            printf("%s\n", redirections[j+1]);
             if (strcmp(redirection, ">") == 0) {
                 *output_fd = open(redirections[j+1], O_WRONLY | O_CREAT);
                 if (*output_fd == -1) {
@@ -313,7 +330,7 @@ void redirect_file(char* tokens, char* tmp_string, int* input_fd, int* output_fd
     tmp_string = NULL;   
 }
 
-void exec_pipe(Pipe* pipes, int pipe_count) {
+void exec_pipe(UPrompt* u, Pipe* pipes, int pipe_count) {
     int task_pipe_fd[2];
     int input_fd = -1;
     int output_fd = -1;
@@ -400,13 +417,13 @@ void exec_pipe(Pipe* pipes, int pipe_count) {
     close(task_pipe_fd[1]);
     if (output_fd == -1) {
         int nbytes;
-        int total_bytes_read = strlen(s.server_response);
-        while ((nbytes = read(task_pipe_fd[0], s.server_response + total_bytes_read, SIZE - total_bytes_read)) > 0) {
+        int total_bytes_read = strlen(u->response);
+        while ((nbytes = read(task_pipe_fd[0], u->response + total_bytes_read, SIZE - total_bytes_read)) > 0) {
             total_bytes_read += nbytes;
         }
         if (nbytes == -1) {
             perror(RED "Error: read()" RESET);
-            exit(EXIT_FAILURE);
+            halt(EXIT_FAILURE);
         }
     }
     close(task_pipe_fd[0]);
@@ -421,39 +438,82 @@ void exec_pipe(Pipe* pipes, int pipe_count) {
     close(output_fd);
 }
 
-void exec_task(Task* task) {
-    exec_pipe(task->pipes, task->pipe_count - 1);
+void exec_task(UPrompt* u, Task* task) {
+    exec_pipe(u, task->pipes, task->pipe_count - 1);
 }
 
-void process_request(void) {
-    memset(s.server_response, 0, sizeof(s.server_response));  // Clear response buffer
+void process_request(UPrompt* u) {
+    memset(u->response, 0, sizeof(u->response));  // Clear response buffer
 
-    for (int i = 0; i < u.task_count && u.tasks[i].tokens != NULL; i++) {
-        exec_task(&u.tasks[i]);
+    for (int i = 0; i < u->task_count && u->tasks[i].tokens != NULL; i++) {
+        exec_task(u, &u->tasks[i]);
     }
-    clear_uprompt();
+    clear_uprompt(u);
 }
 
 void recv_eval_req_loop(void) {
     while (true) {
-        send_dprompt();
+        send_dprompt(&clt);
 
-        if (receive() != 0) {
+        if (receive(&clt) != 0) {
             break;
         }
         else {
-            printf("%s\n", u.client_request);
-            parse_request();
-            process_request();
-            if (strlen(s.server_response) == 0) {
-                strcpy(s.server_response, "\r");
+            // printf("%s\n", clt.request);
+            parse_request(&clt);
+            process_request(&clt);
+            if (strlen(clt.response) == 0) {
+                strcpy(clt.response, "\r");
             }
-            send(s.client_socket, s.server_response, strlen(s.server_response), 0);
-        }            
+            send(s.client_socket, clt.response, strlen(clt.response), 0);
+        }
     }
 }
 
-void server(char* port, char* addr, char* socket_path) {   
+void* cli_input(void* arg) {
+    (void)arg;
+    while (true) {
+        print_dprompt(&cli);
+
+        ssize_t bytes_read;
+        size_t uprompt_len;
+        char* request = NULL;
+
+        fflush(stdin);
+        if ((bytes_read = getline(&request, &uprompt_len, stdin)) == -1) {
+            perror(RED "Error: getline()" RESET);
+            halt(EXIT_FAILURE);
+        }
+        request[bytes_read-1] = '\0'; // remove '\n'
+
+        strncpy(cli.request, request, SIZE - 1);
+        
+        if (strcmp(cli.request, "help") == 0) {
+            help();
+            memset(cli.request, 0, strlen(cli.request));  // Clear buffer
+            print_dprompt(&cli);
+        }
+        else if (strcmp(cli.request, "halt") == 0) {
+            halt(EXIT_SUCCESS);
+        }
+        else {
+            parse_request(&cli);
+            process_request(&cli);
+            if (strlen(cli.response) != 0) {
+                printf("%s", cli.response);
+            }
+        }
+    }
+    return NULL;
+}
+
+void server(char* port, char* addr, char* socket_path) { 
+    pthread_t input_thread;
+    if (pthread_create(&input_thread, NULL, cli_input, NULL) != 0) {
+        perror(RED "Error: pthread_create()" RESET);
+        halt(EXIT_FAILURE);
+    }
+
     bind_socket(port, addr, socket_path);
 
     while (true) {
@@ -461,6 +521,8 @@ void server(char* port, char* addr, char* socket_path) {
 
         recv_eval_req_loop();
     }
+
+    pthread_join(input_thread, NULL);
 
     halt(EXIT_SUCCESS);
 }
