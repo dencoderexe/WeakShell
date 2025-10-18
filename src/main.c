40e84d9773b4d64a482b76c692a15fde6d92a2d2
dencoderexe <dencoder.exe@gmail.com>
2025-10-18 17:21:15 +0200

    add screenshots/gifs

--- src/main.c
+++ src/main.c
@@ -1,37 +1,5 @@
 // DenCoder.EXE
 
-// Assignment #2
-// Author: Denis Danilov
-// Assignment text:
-    // Napíšte v jazyku C jednoduchý interaktívny program, "shell", ktorý bude opakovane čakať na
-    // zadanie príkazu a potom ho spracuje. Na základe princípov klient-server architektúry tak musí
-    // s pomocou argumentov umožňovať funkciu servera aj klienta. Program musí umožňovať
-    // spúšťať zadané príkazy a bude tiež interpretovať aspoň 4 z nasledujúcich špeciálnych
-    // znakov: # ; < > | \ . Príkazy musí byť možné zadať zo štandardného vstupu a tiež zo spojení
-    // reprezentovaných soketmi. Na príkazovom riadku musí byť možné špecifikovať prepínačom -
-    // p port číslo portu a/alebo prepínačom -u cesta názov lokálneho soketu na ktorých bude
-    // program čakať na prichádzajúce spojenia. Po spustení s prepínačom -h sa musia vypísať
-    // informácie o autorovi, účele a použití programu, zoznam príkazov. "Shell" musí poskytovať
-    // aspoň nasledujúce interné príkazy: help - výpis informácií ako pri -h, quit - ukončenie
-    // spojenia z ktorého príkaz prišiel, halt - ukončenie celého programu.
-    // Prednastavený prompt musí pozostávať z mena používateľa, názvu stroja, aktuálneho času a
-    // zvoleného ukončovacieho znaku, e.g. '16:34 user17@student#'. Na zistenie týchto informácií
-    // použite vhodné systémové volania s použitím knižničných funkcií. Na formátovanie výstupu,
-    // zistenie mena používateľa z UID a pod. môžte v programe využiť bežné knižničné funkcie.
-    // Spúšťanie príkazov a presmerovanie súborov musia byť implementované pomocou príslušných
-    // systémových volaní. Tie nemusia byť urobené priamo (cez asembler), avšak knižničná
-    // funkcia popen(), prípadne podobná, nesmie byť použitá. Pri spustení programu bez
-    // argumentov, alebo s argumentom "-s" sa program bude správať vyššie uvedeným spôsobom,
-    // teda ako server. S prepínačom "-c" sa bude správať ako klient, teda program nadviaže
-    // spojenie so serverom cez socket, do ktorého bude posielať svoj štandardný vstup a čítať dáta
-    // pre výstup. Chybové stavy ošetrite bežným spôsobom. Počas vytvárania programu (najmä
-    // kompilácie) sa nesmú zobrazovať žiadne varovania a to ani pri zadanom prepínači
-    // prekladača -Wall.
-    // Vo voliteľných častiach zadania sa očakáva, že tie úlohy budú mať vaše vlastné riešenia, nie
-    // jednoduché volania OS.
-// Deadline: 20.04.2025, 23:59
-// Second year, 2024/2025, summer semester, computer science
-
 // Include necessary libraries for various functions and settings
 #include <stdio.h>
 #include <stdbool.h>
@@ -159,29 +127,3 @@ int main(int argc, char* argv[]) {
 
     return 0; // Return code (though unreachable due to exit)
 }
-
-// Self-evaluation:
-    // The program is in a working state and represents a simple version of shell using system calls to perform tasks. 
-    // The user can select the client or server version of the program at initial startup using command line flags, and connect 
-    // according to the specified parameters. The parameters can be a port, an IP address or a local UNIX socket. In case the user 
-    // does not specify any data, the server and client will by default listen and try to connect to the address 127.0.0.1:50000.
-
-    // The program is written in C using UNIX (Linux) system calls and libraries and runs on linux systems (FreeBSD has not been tested). 
-    // A Makefile is written to compile the program, which can be used to quickly and conveniently compile an entire project.
-
-    // Unfortunately I did not succeed in implementing the abort n function, which could allow to disconnect the user from the server, 
-    // as well as did not manage to fully implement the cd function, it works fine in the case of the client (the request comes from the client), 
-    // but does not work with requests coming from the server (cli mode). 
-
-    // Otherwise, the program should behave stably, during my testing, except for the above described problems, I did not encounter anything. 
-    // I believe that the work is done at a decent level and is a good start for the realization of a full-formed project, which I would like 
-    // to continue when I have free time, I liked the task.
-// Completed bonus tasks:
-    // 3. (3 body) Interný príkaz stat vypíše zoznam všetkých aktuálnych spojení na ktorých
-    // prijíma príkazy, prípadne aj všetky sokety na ktorých prijíma nové spojenia.
-    // 7. (2 body) S prepínačom "-i" bude možné zadať aj IP adresu na ktorej bude program
-    // očakávať spojenia (nielen port).
-    // 11. (2 body) Jeden z príkazov bude využívať funkcie implementované v samostatnej
-    // knižnici, ktorá bude "prilinkovaná" k hlavnému programu.
-    // 21. (2 body) Funkčný Makefile.
-    // 23. (1 bod) Dobré komentáre, resp. rozšírená dokumentácia, v anglickom jazyku.
